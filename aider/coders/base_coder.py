9174307f1de8dfccc5ae81275a362fb6a5b38106
Mateusz Drużny <158189350+Mokofacza@users.noreply.github.com>
2026-02-19 11:22:17 +0100

    rename

--- aider/coders/base_coder.py
+++ aider/coders/base_coder.py
@@ -206,7 +206,7 @@ class Coder:
 
     def get_announcements(self):
         lines = []
-        lines.append(f"Aider v{__version__}")
+        lines.append(f"CisiCode v{__version__}")
 
         # Model
         main_model = self.main_model
